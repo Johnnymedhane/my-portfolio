d7d46cbcfce19df0698d79a59ba460cdcc42ea37
johnny <johnnnymedhane@gmail.com>
2025-03-21 03:04:23 +0200

    deleted animations to articles. added transition insted

--- src/Projects.js
+++ src/Projects.js
@@ -105,7 +105,7 @@ export function SelectItem({ project, i, onClickItem }) {
   );
 }
 export function ProjectsList({ projects, selectItem }) {
-  const [projectsList, setProjectsList] = useState(projects.slice(9));
+  const [projectsList, setProjectsList] = useState(projects.slice(8));
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
--- src/Skills.js
+++ src/Skills.js
@@ -3,14 +3,15 @@
 
 export function Skills() {
   const skills = [
-    { name: 'Github', width: '100', img: 'https://th.bing.com/th/id/OIP.jaHwmagXkJZBZXWOOBlfjAHaD4?rs=1&pid=ImgDetMain' },
+    { name: 'GitHub', width: '100', img: 'https://th.bing.com/th/id/OIP.jaHwmagXkJZBZXWOOBlfjAHaD4?rs=1&pid=ImgDetMain' },
     { name: 'Sql', width: '100', img: 'https://static.frontendmasters.com/resources/2018-03-27-sql/thumb.jpg' },
     { name: 'HTML5', width: '90', img: 'https://th.bing.com/th/id/OIP.fbso8uQcHJtJfO_G8bMacQHaDr?rs=1&pid=ImgDetMain' },
     { name: 'CSS', width: '80', img: 'https://logospng.org/download/css-3/logo-css-3-2048.png' },
-    { name: 'JavaScript', width: '70', img: 'https://logos-download.com/wp-content/uploads/2019/01/JavaScript_Logo.png' },
-    { name: 'Python', width: '60', img: 'https://anturis.com/wp-content/uploads/2022/09/Python-Logo-1.png' },
+    { name: 'JavaScript', width: '80', img: 'https://logos-download.com/wp-content/uploads/2019/01/JavaScript_Logo.png' },
     { name: 'React', width: '70', img: 'https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png' },
+    { name: 'Python', width: '60', img: 'https://anturis.com/wp-content/uploads/2022/09/Python-Logo-1.png' },
     { name: 'Git', width: 60, img: 'https://miro.medium.com/max/3840/1*fDwzjCH3qzhosC6DVel4ng.jpeg' },
+
   ];
   return (
    
