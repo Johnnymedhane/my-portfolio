dcb2d194dec348544fb08ef1fa58dd92f212929d
johnny <johnnnymedhane@gmail.com>
2025-06-02 19:12:13 +0300

    added routers

--- src/App.js
+++ src/App.js
@@ -1,79 +1,38 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import  { useEffect, useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-// import { Mybackground } from './myStory';
-import { Header } from "./Header";
-import { Main } from "./Main";
-import { Hero } from "./Hero";
-// import { About } from "./About";  // Import the About component
-// import { Skills } from "./Skills"; // Import the Skills component
-// import { Projects } from "./Projects"; // Import the Projects component
-// import { Resume } from "./Resume"; // Import the Resume component
-// import { Contact } from "./Contact"; // Import the Contact component
+import HomePage from './pages/HomePage';
+import { AppProvider } from './contextApi';
+import { Mybackground } from './pages/myStory';
+import HeroSection from './pages/HeroSection';
+import ContactSection from './pages/ContactSection';
+import ProjectsSecction from './pages/ProjectsSecction';
+import ResumeSection from './pages/Resume.Section';
+import SkillsSection from './pages/SkillsSection';
+import AboutSection from './pages/AboutSection';
 
 export const navList = ['home', 'about', 'skills', 'projects', 'resume', 'contact'];
 
 function App() {
-  const [showIndicator, setShowIndicator] = useState(true);
-  const [selectedNav, setSelectedNav] = useState("home");
-  const title = 'JohnnyMe';
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 70) {
-        setShowIndicator(false);
-      } else {
-        setShowIndicator(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {
-    function changTitle() {
-      document.title = `${title} - ${selectedNav}`;
-    }
-    changTitle();
-    return () => {
-      document.title = title;
-    };
-  }, [selectedNav]);
-
+ 
   return (
-      <div className="App">
-        <Header selectedNav={selectedNav} setSelectedNav={setSelectedNav} />
-        <Hero showIndicator={showIndicator} setSelectedNav={setSelectedNav} />
-         <Main selectedNav={selectedNav} setSelectedNav={setSelectedNav} />
-      </div>
-  
+   <AppProvider className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HeroSection />} />
+          <Route path="/about" element={<AboutSection />} />
+          <Route path="/skills" element={<SkillsSection />} />
+          <Route path="/projects" element={<ProjectsSecction />} />
+          <Route path="/resume" element={<ResumeSection />} />
+          <Route path="/contact" element={<ContactSection />} />
+          <Route path='/my-story' element={<Mybackground />} />
+      </Routes>
+      
+      </BrowserRouter>
+      </AppProvider>
+
   );
 }
 
-/*function ConditionalHeader({ selectedNav, setSelectedNav }) {
-  const location = useLocation();
-  const hideHeaderPaths = ['/my-story'];
 
-  if (hideHeaderPaths.includes(location.pathname)) {
-    return null;
-  }
-
-  return <Header selectedNav={selectedNav} setSelectedNav={setSelectedNav} />;
-}*/
-
-
-/*function ConditionalHero( { showIndicator, setSelectedNav }) {
-  const location = useLocation();
-  const hideHeaderPaths = ['/my-story'];
-
-  if (hideHeaderPaths.includes(location.pathname)) {
-    return null;
-  }
-
-  return <Hero showIndicator={showIndicator} setSelectedNav={setSelectedNav} />;
-}*/
 export default App;
--- src/Loading.js
+++
@@ -1,9 +0,0 @@
-export function Loading() {
-  return (
-    <div className="loading">
-      <div className="loading-icon">
-        <i className="fa-solid fa-spinner"></i>
-      </div>
-    </div>
-  );
-}
