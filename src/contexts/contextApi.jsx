e54f23e56292a01b1088a0e073ab38144f1cc1b5
johnny <johnnnymedhane@gmail.com>
2026-04-07 02:03:58 +0300

    fix dark mode conflict

--- src/contexts/contextApi.jsx
+++ src/contexts/contextApi.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import useLocalStorage from "../hooks/useLocalStorage";
+// import useLocalStorage from "../hooks/useLocalStorage";
 
 const AppContext = createContext();
 
