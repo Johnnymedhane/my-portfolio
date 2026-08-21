369f38125ced40ceda8d108d11306ca1bd4454d4
johnny <johnnymedhane@gmail.com>
2026-08-22 02:13:39 +0300

    update cv

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -56,9 +56,10 @@ export function About() {
         </p>
         <p>
           I work mainly with <b>React</b>, <b>Next.js</b>, <b>JavaScript</b>,{" "}
-          <b>Node.js</b>, and <b>Supabase</b>, styling with <b>Tailwind CSS</b>{" "}
-          and <b>CSS Modules</b>, and I’m continuously improving my skills
-          through real-world projects, problem-solving, and hands-on learning.
+          <b>TypeScript</b>, <b>Node.js</b>, and <b>Supabase</b>, styling with{" "}
+          <b>Tailwind CSS</b> and <b>CSS Modules</b>, and I’m continuously
+          improving my skills through real-world projects, problem-solving, and
+          hands-on learning.
         </p>
 
         <p>
